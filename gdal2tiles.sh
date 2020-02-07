1d035b9038124066206f28ee32ef61b54c8ac81e
pigr3co <pigrecoinfinito@gmail.com>
2020-02-07 21:24:07 +0100

    update zoom

--- gdal2tiles.sh
+++ gdal2tiles.sh
@@ -2,5 +2,5 @@
 
 set -x
 
-gdal2tiles.py --zoom=7-11 ./index50kP2.tif
-gdal2tiles.py --zoom=7-11 ./index25kP2.tif
+gdal2tiles.py --zoom=6-11 ./index50kP2.tif
+gdal2tiles.py --zoom=6-11 ./index25kP2.tif
